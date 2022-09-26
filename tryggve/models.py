f45b792bd4dcaccac4f1c8fc0a74c7e6c226653f
Arvid Harder <arvid.harder@gmail.com>
2022-09-26 13:46:55 +0200

    refactoring code

--- tryggve/models.py
+++ tryggve/models.py
@@ -7,9 +7,22 @@ static_url = settings.STATIC_URL
 class Country(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     description = models.CharField(max_length=1000)
     in_country = models.ForeignKey(Country, on_delete=models.CASCADE)
     image = models.URLField(max_length=200)
+    affliation = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.first_name
+
+class Icon(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.URLField(max_length=200)
+    def __str__(self):
+        return self.name
--- tryggve/urls.py
+++ tryggve/urls.py
@@ -3,21 +3,20 @@ from tryggve import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Glyph page and homepage
     path('', TemplateView.as_view(template_name="tryggve/tryggve.html"), name="tryggve-view"),
+    
+    # Urls to the navbar
     path('about/', TemplateView.as_view(template_name="tryggve/about.html"), name="about-view"),
-    path('news/', TemplateView.as_view(template_name="tryggve/news.html"), name="news-view"),
-    path('participating-countries/', TemplateView.as_view(template_name="tryggve/participating_countries.html"), name="participating-view"),
+    path('resources/', TemplateView.as_view(template_name="tryggve/resources.html"), name="resources-view"),
+    path('team/<int:pk>/', views.CountryView.as_view(), name="country-view"),
     path('infrastructure/', TemplateView.as_view(template_name="tryggve/infrastructure.html"), name="infrastructure-view"),
     path('funding/', TemplateView.as_view(template_name="tryggve/funding.html"), name="funding-view"),
     path('projects/', TemplateView.as_view(template_name="tryggve/projects.html"), name="projects-view"),
-    path('test/', TemplateView.as_view(template_name="tryggve/test.html"), name="test-view"),
-    path('resources/', TemplateView.as_view(template_name="tryggve/resources.html"), name="resources-view"),
+    path('news/', TemplateView.as_view(template_name="tryggve/news.html"), name="news-view"),
     
+    # for testing stufff
+    path('test/', views.TestView.as_view(), name="test-view"),
+    path('test2/', TemplateView.as_view(template_name="tryggve/PRS_in_S3.html"), name="test2-view"),
     
-    path('swe/', TemplateView.as_view(template_name="tryggve/sweden.html"), name="sweden-view"),
-    path('den/', TemplateView.as_view(template_name="tryggve/denmark.html"), name="denmark-view"),
-    path('nor/', TemplateView.as_view(template_name="tryggve/norway.html"), name="norway-view"),
-    path('est/', TemplateView.as_view(template_name="tryggve/estonia.html"), name="estonia-view"),
-    
-
 ]
